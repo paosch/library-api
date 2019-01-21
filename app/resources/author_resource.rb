c78193d4f80be89bbbbb0188244aeafb5de0655f
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-01-21 08:54:03 +0000

    Filter brings author by last name only

--- app/resources/author_resource.rb
+++ app/resources/author_resource.rb
@@ -1,3 +1,13 @@
 class AuthorResource < JSONAPI::Resource
   attributes :first, :last
+  filter :query
+
+  def self.apply_filter(records, filter, value, options)
+    case filter
+      when :query
+        records.where({last: value.first})
+      else 
+        super
+    end
+  end
 end
